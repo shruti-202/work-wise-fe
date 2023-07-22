e2462c02256701fc658302578af85b1b5a8c08d4
Shruti <shrutigawande1910@gmail.com>
2023-07-22 10:44:35 +0530

    fix : BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowswerRouter} from "react-router-dom"
+import {BrowserRouter} from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowswerRouter>
+    <BrowserRouter>
     <App />
-    </BrowswerRouter>
+    </BrowserRouter>
     
   </React.StrictMode>,
 )
