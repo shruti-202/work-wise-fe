45cc30bc8f17a73c41f7d2398e5101dac94de152
Shruti <shrutigawande1910@gmail.com>
2023-07-26 21:25:55 +0530

    feat : Implemented Footer

---
+++ src/components/main/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Footer.css";
+
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>
+          Made with ❤️ by <a href="https://github.com/shruti-202" target="_blank"><strong>Shruti Gawande </strong></a> in India 
+        </p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
