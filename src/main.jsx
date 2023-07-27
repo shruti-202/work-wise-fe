ef1849d47c708eb69dbbf0d790180c66e5c860c4
Shruti <shrutigawande1910@gmail.com>
2023-07-28 01:27:45 +0530

    style: updated register form styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter} from "react-router-dom";
+import { SnackbarProvider } from 'notistack';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <SnackbarProvider maxSnack={3}>
     <App />
+    </SnackbarProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
--- src/pages/RegisterUserPage.jsx
+++ src/pages/RegisterUserPage.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import {useSnackbar} from "notistack";
 
 function RegisterUserPage() {
+  
+const {enqueueSnackbar}=useSnackbar();
+
+const handleSubmit=(e)=>{
+  e.preventDefault();
+
+enqueueSnackbar("This is a success message!");
+}
   return (
     <div className="register">
       <div className="register-container">
         <div className="register-form">
           <h1>Sign Up</h1>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <input type="text" className="input-field"  placeholder="Full Name" required/>
             <input type="number" className="input-field" placeholder="Phone No" required/>
             <input type="email" className="input-field" placeholder="Email" required/>
